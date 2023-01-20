0d8375cfed18b9b591c3a07024425fd631d87303
thinhbui2911 <thinhbui2911@gmail.com>
2023-01-19 16:21:06 -0800

    first commit

---
+++ models.py
@@ -0,0 +1,49 @@
+"""Models for Blogly."""
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/img/1667"
+
+
+class User(db.Model):
+    """user site."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+    @property
+    def full_name(self):
+        """Return user full name."""
+
+        return f"{self.first_name} {self.last_name}"
+
+class Post(db.Model):
+    """Blog post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Return formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+def connect_db(app):
+    """Connect to database"""
+    db.app = app
+    db.init_app(app)
